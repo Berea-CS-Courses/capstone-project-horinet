924341baa9da4259c026dd23d6a6a4bc6c612ad7
horinet <horinet@berea.edu>
2021-04-22 13:47:15 -0400

    just committing because android studio is crashing.

--- Todolistproject/app/src/main/java/com/example/todolist/FirebaseStorage.java
+++ Todolistproject/app/src/main/java/com/example/todolist/FirebaseStorage.java
@@ -1,4 +1,26 @@
 package com.example.todolist;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+// I am not even close to done with this. I've done a lot of reading and I'm going to attempt to try this out and see if I can get it to work with firebase storing data and also user authentication
 public class FirebaseStorage {
+    private static final String TAG = "EmailPassword";
+    // [START declare_auth]
+    private FirebaseAuth uAuth;
+    @Override
+    public void onCreate(Bundle SavedInstanceState){
+        super.onCreate(SavedInstanceState);
+        uAuth = FirebaseAuth.getInstance();
+        //initializes the firebase auth
+    }
+
 }
