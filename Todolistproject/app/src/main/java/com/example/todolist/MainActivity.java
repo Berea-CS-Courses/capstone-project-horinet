87c300bac572bf50c0ee83e2cb442781e9d44d51
horinet <horinet@berea.edu>
2021-04-22 13:24:32 -0400

    I've added a log in screen, a class for firebase activities, haven't started coding that in just yet but will go to that next. Trying to clean up my code as I go!

--- Todolistproject/app/src/main/java/com/example/todolist/MainActivity.java
+++ Todolistproject/app/src/main/java/com/example/todolist/MainActivity.java
@@ -22,9 +22,12 @@ public class MainActivity extends Activity {
     private Button additem;
     private ImageButton calendar;
     private Button addtaskhs;
+    private Button logscreen;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
+        //This is the function that controls what happens on create. The buttons have onclick listeners that take the user to the corresponding page.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homescreen);
         todo = findViewById(R.id.todobutton);
@@ -41,15 +44,15 @@ public class MainActivity extends Activity {
     }
 
     public void onTodoScreen(View v){
+        //creates an array of the items on the todo list.
+        //I will also update this with the addtaskscreen.xml file.
         setContentView(R.layout.todo_screen);
-        readItems(); // <---- Add this line
+        readItems();
         lvItems = findViewById(R.id.lvItems);
         items = new ArrayList<String>();
         itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
         items.add("First Item");
-        //items.add("Second Item");
-        //items.add("Third Item Item");
         // Setup remove listener method call
         additem = findViewById(R.id.btnAddItem);
         additem.setOnClickListener(this::onAddItem);
@@ -69,10 +72,12 @@ public class MainActivity extends Activity {
                 });
     }
     public void calendarView(View v){
+        //just the calendar view that's being put on the back burner right now.
         setContentView(R.layout.calendarview);
     }
 
     public void onAddItem(View v) {
+        //adds the item to the list from the todoscreen, probably not using this in the end I will be making changes to implement it with the addtaskscreen xml file
         EditText etNewItem = findViewById(R.id.etNewItem);
         String itemText = etNewItem.getText().toString();
         itemsAdapter.add(itemText);
@@ -80,6 +85,7 @@ public class MainActivity extends Activity {
         writeItems();
     }
     private void readItems() {
+        //also used this for the prototype but will either need to change it or delete it. Reads the items in the todo list
         File filesDir = getFilesDir();
         File todoFile = new File(filesDir, "todo.txt");
         try {
@@ -90,6 +96,7 @@ public class MainActivity extends Activity {
     }
 
     private void writeItems() {
+        //also used this for the prototype but will either need to change it or delete it. writes the items in the todo list
         File filesDir = getFilesDir();
         File todoFile = new File(filesDir, "todo.txt");
         try { FileUtils.writeLines(todoFile, items); }
